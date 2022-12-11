2340dbc47165c4d88a5dc64a5844a32333d2d3f5
AIE\s214879 <s214879@srtudents.aie.edu.au>
2022-12-12 10:23:04 +1030

    Initial Design

--- Assets/Scripts/UI/UI_HUD.cs
+++ Assets/Scripts/UI/UI_HUD.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_HUD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Button levelDesignButton;
+    [SerializeField] Button characterDesignButton;
+    [SerializeField] Button programingDesignButton;
+    [SerializeField] Button testingButton;
+    [SerializeField] Button publishButton;
+
+    private void Awake()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        levelDesignButton.onClick.AddListener(OnLevelDesignButtonPress);
+        characterDesignButton.onClick.AddListener(OnCharacterDesignButtonPress);
+        programingDesignButton.onClick.AddListener(OnProgramingDesignButtonPress);
+        testingButton.onClick.AddListener(OnTestingButtonnPress);
+        publishButton.onClick.AddListener(OnPublishButtonPress);
+    }
+
+    private void OnLevelDesignButtonPress()
+    {
+
+    }
+
+    private void OnCharacterDesignButtonPress()
+    {
+
+    }
+
+    private void OnProgramingDesignButtonPress()
+    {
+
+    }
+
+    private void OnTestingButtonnPress()
+    {
+
+    }
+
+    private void OnPublishButtonPress()
+    {
+
     }
 }
