133c8924784b03169bd85362afe8132ce00ade40
AIE\s214879 <s214879@srtudents.aie.edu.au>
2022-12-12 20:18:09 +1030

    Working on programming menu

--- Assets/Scripts/Character/CharacterConfig.cs
+++ Assets/Scripts/Character/CharacterConfig.cs
@@ -9,4 +9,5 @@ public class CharacterConfig : ScriptableObject
     [field: SerializeField] public int helmetRef { get; set; }
     [field: SerializeField] public int armourColourRef { get; set; }
     [field: SerializeField] public int weaponRef { get; set; }
+    [field: SerializeField] public List<string> behaviours { get; set; }
 }
