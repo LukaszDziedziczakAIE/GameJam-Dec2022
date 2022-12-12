c9483f9279ca19e383a68e7908f44a135fde1a13
AIE\s214879 <s214879@srtudents.aie.edu.au>
2022-12-12 11:43:30 +1030

    Level Design UI

---
+++ Assets/Scripts/UI/UI_Base.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Base : MonoBehaviour
+{
+    [SerializeField] protected GameController Game;
+
+    private void Awake()
+    {
+        if (Game == null) Game = FindObjectOfType<GameController>();
+    }
+
+    public virtual void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public virtual void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/UI_LeveDesign.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_LeveDesign : UI_Base
+{
+    [SerializeField] Button enviromentButton;
+    [SerializeField] Button interactableButton;
+    [SerializeField] Button characterButton;
+    [SerializeField] RectTransform enviromentTab;
+    [SerializeField] RectTransform interactableTab;
+    [SerializeField] RectTransform characterTab;
+
+    private void Start()
+    {
+        enviromentButton.onClick.AddListener(OnEnviromentButtonPress);
+        interactableButton.onClick.AddListener(OnInteractableButtonPress);
+        characterButton.onClick.AddListener(OnCharacterButtonPress);
+
+        enviromentTab.gameObject.SetActive(false);
+        interactableTab.gameObject.SetActive(false);
+        characterTab.gameObject.SetActive(false);
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        OnEnviromentButtonPress();
+    }
+
+    private void OnEnviromentButtonPress()
+    {
+        enviromentTab.gameObject.SetActive(true);
+        interactableTab.gameObject.SetActive(false);
+        characterTab.gameObject.SetActive(false);
+    }
+
+    private void OnInteractableButtonPress()
+    {
+        enviromentTab.gameObject.SetActive(false);
+        interactableTab.gameObject.SetActive(true);
+        characterTab.gameObject.SetActive(false);
+    }
+
+    private void OnCharacterButtonPress()
+    {
+        enviromentTab.gameObject.SetActive(false);
+        interactableTab.gameObject.SetActive(false);
+        characterTab.gameObject.SetActive(true);
+    }
+}
