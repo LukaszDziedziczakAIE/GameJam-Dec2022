8c7d349cc11a00baa58fff60a7832f21762a4ef8
AIE\s214879 <s214879@srtudents.aie.edu.au>
2022-12-12 18:28:44 +1030

    Added Character Config

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -6,8 +6,12 @@ public class Character : MonoBehaviour
 {
     [field: SerializeField] public CharacterController controller;
 
+    public CharacterConfig Config { get; private set; }
+
     private void Awake()
     {
         if (controller == null) controller = GetComponent<CharacterController>();
     }
+
+    public void SetNewConfig(CharacterConfig newConfig) { Config = newConfig; }
 }
