68a7003ddd29c2b89998a9a297622cac440bec79
AIE\s213321 <s213321@students.aie.edu.au>
2022-12-13 15:37:05 +1030

    Animaton events readded ,Updates to Weapon audio and handler script

--- Assets/Scripts/Character/CharacterAnimEventHandler.cs
+++ Assets/Scripts/Character/CharacterAnimEventHandler.cs
@@ -23,8 +23,43 @@ public class CharacterAnimEventHandler : MonoBehaviour
         audio?.PlayAttackVoice();
     }
 
+    private void PlayReactionVoice()
+    {
+        audio?.PlayReactionVoice();
+    }
+
+    private void PlayDeathVoice()
+    {
+        audio?.PlayDeathVoice();
+    }
+
     private void PlayJumpVoice()
     {
         audio?.PlayJumpVoice();
+    }
+
+    private void PlayWoosh()
+    {
+        character?.Weapon?.Audio?.PlayWoosh();
+    }
+
+    private void PlaySwing()
+    {
+        character?.Weapon?.Audio?.PlaySwing();
+    }
+
+    private void PlayImpact()
+    {
+        character?.Weapon?.Audio?.PlayImpact();
+    }
+
+    private void ColiderOn()
+    {
+        character.Weapon.BoxCollider.enabled = true;  
+    }
+    private void ColiderOff()
+    {
+        character.Weapon.BoxCollider.enabled = false;
+
     }
 }
