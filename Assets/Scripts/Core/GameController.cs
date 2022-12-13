ff6a48cdfb29488893ecf54f142ad7c90b921ce0
AIE\s210837 <s210837@students.aie.edu.au>
2022-12-13 14:37:07 +1030

    Change in Game Manager

--- Assets/Scripts/Core/GameController.cs
+++ Assets/Scripts/Core/GameController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     [field: SerializeField] public UI_HUD HUD { get; private set; }
+    [field: SerializeField] public PointSystem PointSystem { get; private set; }
     [field: SerializeField] public CodeBlockDetector BlockDetector { get; private set; }
     [field: SerializeField] public CameraController Camera { get; private set; }
     [field: SerializeField] public PlayerCharacter PlayerCharacter { get; private set; }
